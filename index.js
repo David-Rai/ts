c31dd88e459ff46d516e26fae78cc573863cf996
david-rai <davidkhaling123@gmail.com>
2025-06-18 21:52:18 +0545

    setup  implemented

---
+++ index.js
@@ -0,0 +1,12 @@
+//Variable declaration in ts
+var rollno = 12;
+var nam = "ram";
+var isValid = false;
+var demo = "hey";
+console.log(demo);
+demo = 12;
+console.log(demo);
+// console.log("hey there jane man")
+// console.log(nam)
+// console.log(rollno)
+// console.log(isValid)
---
+++ index.ts
@@ -0,0 +1,14 @@
+
+//Variable declaration in ts
+const rollno:Number=12
+const nam:String = "ram"
+const isValid:Boolean = false
+let demo:any = "hey"
+
+console.log(demo)
+demo=12
+console.log(demo)
+// console.log("hey there jane man")
+// console.log(nam)
+// console.log(rollno)
+// console.log(isValid)
