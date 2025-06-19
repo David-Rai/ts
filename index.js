c47a19fe27bb3f49df2fd4435e755c2cb9266ec7
david-rai <davidkhaling123@gmail.com>
2025-06-19 20:24:15 +0545

    Interface done

--- index.js
+++ index.js
@@ -1,49 +1,16 @@
-//Simple types in ts
-var rollno = 12; //for number datatype
-var nam = "ram"; //for string datatype
-var isValid = false; //for bool datatype
-//Arrays
-var names = ["ram", "la", "c", "1", 2, 3];
-// console.log(names)
-//Specials types
-var value;
-var demo = "hey"; //for all type of datatype
-var demo2;
-var demo3;
-var demo4;
-var func1 = function (message) {
-    console.log(message);
+// //Simple types in ts
+// const rollno:Number=12//for number datatype
+// const nam:String = "ram"//for string datatype
+// const isValid:Boolean = false//for bool datatype
+var user = {
+    name: "ram",
+    age: 30,
 };
-//Tuple - a array of predefined length and type of element in a array
-var ourTuple = [1, "ram", true];
-// ourTuple=[1,"ram",false]
-// ourTuple.push("ram2")
-//Objects
-var car = {
-    name: "toyota",
-    rollno: 3,
-    isValid: false
+var greet = function (name) {
+    return name;
 };
-//enum
-var Size;
-(function (Size) {
-    Size[Size["Small"] = 0] = "Small";
-    Size[Size["Medium"] = 1] = "Medium";
-    Size[Size["Large"] = 2] = "Large";
-})(Size || (Size = {}));
-var Car;
-(function (Car) {
-    Car["name"] = "toyota";
-    Car["prize"] = "11111";
-    Car["address"] = "pulchowk";
-})(Car || (Car = {}));
-//Type annotation
-var random;
-random = "ram";
-console.log(random);
-random = 1111;
-console.log(random);
-// function printDetails(detail:Car){
-//     console.log(detail)
-// }
-// console.log(printDetails(Car.name))
+var emp = {
+    name: "ram",
+    salary: 22222
+};
+console.log(emp);
--- index.ts
+++ index.ts
@@ -1,46 +1,82 @@
 
-//Simple types in ts
-const rollno:Number=12//for number datatype
-const nam:String = "ram"//for string datatype
-const isValid:Boolean = false//for bool datatype
+// //Simple types in ts
+// const rollno:Number=12//for number datatype
+// const nam:String = "ram"//for string datatype
+// const isValid:Boolean = false//for bool datatype
 
-//Arrays
-const names:readonly any[]=["ram","la","c","1",2,3]
-// console.log(names)
+// //Arrays
+// const names:readonly any[]=["ram","la","c","1",2,3]
+// // console.log(names)
 
-//Specials types
-let value:unknown
-let demo:any = "hey"//for all type of datatype
-let demo2:undefined
-let demo3:null
-let demo4:never
-let func1=(message:String):void =>{
-console.log(message)
+// //Specials types
+// let value:unknown
+// let demo:any = "hey"//for all type of datatype
+// let demo2:undefined
+// let demo3:null
+// let demo4:never
+// let func1=(message:String):void =>{
+// console.log(message)
+// }
+
+// //Tuple - a array of predefined length and type of element in a array
+// let ourTuple:readonly [number,string,boolean]=[1,"ram",true]
+// // ourTuple=[1,"ram",false]
+// // ourTuple.push("ram2")
+
+// //Objects
+// let car:{name:string,rollno:number,isValid:boolean} = {
+// name:"toyota",
+// rollno:3,
+// isValid:false
+// }
+
+// //enum
+// enum Size {Small,Medium,Large}s
+// enum Car{
+//     name="toyota",
+//     prize="11111",
+//     address="pulchowk"
+// }
+
+// //Type annotation
+// let random:number | string | boolean
+// // random="ram"
+// // console.log(random)
+// // random=1111
+// // console.log(random)
+
+//Interface for user
+interface User {
+    name: String,
+    age: number,
+    isMarried?: boolean
 }
 
-//Tuple - a array of predefined length and type of element in a array
-let ourTuple:readonly [number,string,boolean]=[1,"ram",true]
-// ourTuple=[1,"ram",false]
-// ourTuple.push("ram2")
-
-//Objects
-let car:{name:string,rollno:number,isValid:boolean} = {
-name:"toyota",
-rollno:3,
-isValid:false
+const user: User = {
+    name: "ram",
+    age: 30,
 }
 
-//enum
-enum Size {Small,Medium,Large}
-enum Car{
-    name="toyota",
-    prize="11111",
-    address="pulchowk"
+//Interface for function
+interface Greet{
+    (name:string) : any
+}
+const greet:Greet = (name)=>{
+return name
 }
 
-//Type annotation
-let random:number | string | boolean
-// random="ram"
-// console.log(random)
-// random=1111
-// console.log(random)
+//Extending interface
+interface Person{
+    name:string
+}
+
+interface Employee extends Person{
+    salary:number
+}
+
+const emp:Employee={
+    name:"ram",
+    salary:22222
+}
+
+console.log(emp)
