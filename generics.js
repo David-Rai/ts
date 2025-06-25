0b76497a49e3df4f4982b3f92c323a83773b7931
david-rai <davidkhaling123@gmail.com>
2025-06-25 12:52:46 +0545

    Basic Generics learned with type alias and the interface

--- generics.js
+++ generics.js
@@ -1,7 +1,7 @@
 // //Generics
 // It is like the parameter for string it helps
 // function , interfaces , alias to get types as Parameter
-function logger(age, nam) {
+var logger = function (nam, age) {
     console.log("your name is ".concat(nam, " and your age is ").concat(age));
-}
-logger(12, "ram babu");
+};
+logger("ram", 12);
--- generics.ts
+++ generics.ts
@@ -2,8 +2,53 @@
 // It is like the parameter for string it helps
 // function , interfaces , alias to get types as Parameter
 
-function logger<A,N>(age:A,nam: N ): void {
-console.log(`your name is ${nam} and your age is ${age}`)
+
+//******Generics function******* */
+// function logger<A,N>(age:A [],nam: N ): void {
+// console.log(`your name is ${nam} and your age is ${age}`)
+// }
+
+// logger([12,12,34],<string>"ram babu")
+
+
+//*********Generics types and Interfaces******** */
+// type User<N>={
+// name:N
+// }
+
+// let user={
+//     name:"ram"
+// } as User<string>
+
+// console.log(user)
+
+// type logger<N, A> = (nam: N, age: A) => void
+
+// const logger: logger<string, number> = (nam, age) => {
+//     console.log(`your name is ${nam} and your age is ${age}`)
+// }
+
+// logger("ram", 12)
+
+//Interface with generics
+// interface User<N,A>{
+//     name:N
+//     age:A
+// }
+
+// const user:User<string,number>={
+//     name:"ram",
+//     age:12
+// }
+
+// console.log(user)
+
+interface logger<N,A> {
+    (nam: N, age: A): void
 }
 
-logger(<number>12,<string>"ram babu")
+const logger: logger<string,number> = function (nam, age) {
+    console.log(`your name is ${nam} and your age is ${age}`)
+}
+
+logger("ram", 12)
