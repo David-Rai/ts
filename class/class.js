7c322f2055a780eb5b8add17d6cea8c56f1f69ed
david-rai <davidkhaling123@gmail.com>
2025-06-27 16:54:21 +0545

    Access modifiers in ts with js classes

--- class/class.js
+++ class/class.js
@@ -1,38 +1,43 @@
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        if (typeof b !== "function" && b !== null)
-            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
 //Js classes with TS
-var DOB = /** @class */ (function () {
-    function DOB(date) {
-        this.date = date;
+// class DOB {
+//     protected readonly date: number
+//     constructor(date: number) {
+//         this.date = date
+//     }
+// }
+var User = /** @class */ (function () {
+    function User(name, age) {
+        this.name = name;
+        this.age = age;
     }
-    return DOB;
-}());
-var User = /** @class */ (function (_super) {
-    __extends(User, _super);
-    function User(date, name, age) {
-        var _this = _super.call(this, date) || this;
-        _this.name = name;
-        _this.age = age;
-        return _this;
-    }
+    Object.defineProperty(User.prototype, "nameSet", {
+        //setter for the name
+        set: function (newName) {
+            if (newName.length <= 3) {
+                console.log("name lenght need to be more than 3");
+            }
+            else {
+                this.name = newName;
+            }
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(User.prototype, "nameGet", {
+        //getter for name
+        get: function () {
+            return this.name;
+        },
+        enumerable: false,
+        configurable: true
+    });
     //logger function
     User.prototype.log = function () {
-        console.log("Name ".concat(this.name, " & age ").concat(this.age, " and DOB is ").concat(this.date));
+        console.log("Name ".concat(this.name, " & age ").concat(this.age));
     };
     return User;
-}(DOB));
-var ram = new User(121212, 'Ram', 12);
-ram.log();
+}());
+var ram = new User('Ram', 12);
+console.log(ram);
+ram.nameSet = "asdsadsad";
+console.log(ram.name);
--- class/class.ts
+++ class/class.ts
@@ -1,28 +1,44 @@
 
 //Js classes with TS
-class DOB {
-    protected readonly date: number
-    constructor(date: number) {
-        this.date = date
-    }
-}
+// class DOB {
+//     protected readonly date: number
+//     constructor(date: number) {
+//         this.date = date
+//     }
+// }
 
-class User extends DOB {
-    public name: string
-    private age: number
+class User {
+    name: string
+    age: number
 
-    constructor(date: number, name: string, age: number) {
-        super(date)
+    constructor(name: string, age: number) {
         this.name = name
         this.age = age
+    }
+
+    //setter for the name
+    set nameSet(newName: string) {
+        if (newName.length <= 3) {
+            console.log("name lenght need to be more than 3")
+        } else {
+            this.name = newName
+        }
+    }
+
+
+    //getter for name
+    get nameGet(): string {
+        return this.name
     }
 
 
     //logger function
     log(): void {
-        console.log(`Name ${this.name} & age ${this.age} and DOB is ${this.date}`)
+        console.log(`Name ${this.name} & age ${this.age}`)
     }
 }
 
-const ram = new User(121212,'Ram', 12)
-ram.log()
+const ram = new User('Ram', 12)
+console.log(ram)
+ram.nameSet="asdsadsad"
+console.log(ram.name)
