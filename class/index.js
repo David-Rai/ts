a56fc24e834eab354c212764a1e280bad9ddfca3
david-rai <davidkhaling123@gmail.com>
2025-06-27 14:47:17 +0545

    Getters and Setters done

--- class/index.js
+++ class/index.js
@@ -15,11 +15,35 @@ class Box {
         }
     }
 
-    get h(){
-        return this._h
+    //getter for the height
+    get h() {
+        return this._h.toFixed(2)
     }
+
+    //setter for the width
+    set w(newWidth) {
+        if (newWidth > 0) {
+            return this._w
+        } else {
+            console.error("Please enter the valid number")
+        }
+    }
+
+    //Getters for the width
+    get w() {
+        return this._w.toFixed(2)
+    }
+
+
+    //Getter for calculating the area of the box
+    get area(){
+
+        return this._w * this._h
+    }
+
 }
 
-const box1 = new Box(1, 2)
+const box1 = new Box(18, 2)
 console.log(box1.h)
 console.log(box1.w)
+console.log(box1.area)
