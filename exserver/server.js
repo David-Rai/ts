2b42405eadfd4cb1f402b5f21609a0b3107fdaf6
david-rai <davidkhaling123@gmail.com>
2025-07-03 21:40:43 +0545

    Used watch and concurrrently for the express with typescript

--- exserver/server.js
+++ exserver/server.js
@@ -2,8 +2,7 @@ import express from "express";
 const app = express();
 const port = 1111;
 app.get("/", (req, res) => {
-    res.send("Hello World!");
-    console.log("Response sent");
+    res.json({ message: "heyj there", status: 200 });
 });
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
--- exserver/server.ts
+++ exserver/server.ts
@@ -1,13 +1,12 @@
-import express ,{Request,Response} from "express";
+import express, { Request, Response } from "express";
 const app = express();
 const port = 1111;
 
-app.get("/", (req:Request, res:Response) => {
-  res.send("Hello World!");
-  console.log("Response sent");
+app.get("/", (req: Request, res: Response) => {
+	res.json({message:"heyj there",status:200});
 });
 
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+	console.log(`Example app listening on port ${port}`);
 });
